6e1b762fe82848327e047a45e26fa77e739d1839
cnagbim <cnagbim@gmail.com>
2019-04-14 17:58:15 -0500

    Update clean_merge_energyburden_health_food.R

--- clean_merge_code/code_clean_merge_analysis_data/clean_merge_energyburden_health_food.R
+++ clean_merge_code/code_clean_merge_analysis_data/clean_merge_energyburden_health_food.R
@@ -1,5 +1,8 @@
 #The energy burden data from NREL was seperated into three files (see the readme in energy burden folder)
 #Therefore we need to merge the files vertically
+#Load packages tidyverse
+
+library(tidyverse)
 energy_burden_0<-read.csv("~/GitHub/Data-Management-Final-Project-Agbim/data/data_raw/analysis_data_raw/energy_burden_nrel/Energy Burden_Apr 12 2019.csv")
 
 view(energy_burden_0)
@@ -8,5 +11,12 @@ energy_burden_1<-read.csv("~/GitHub/Data-Management-Final-Project-Agbim/data/dat
 view(energy_burden_1)
 
 energy_burden_2<-read.csv("~/GitHub/Data-Management-Final-Project-Agbim/data/data_raw/analysis_data_raw/energy_burden_nrel/Energy Burden_Apr 12 2019_2.csv")
-
 view(energy_burden_2)
+#Column names match. Matching column/var names is required to merge
+#check what arguements needed to merge datasets
+?rbind()
+
+#merge all energy_burden using rbind function. create new dataframe full_en_burden.
+
+full_en_burden<-rbind(energy_burden_0,energy_burden_1,energy_burden_2)
+view(full_en_burden)
